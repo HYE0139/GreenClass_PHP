d38db8f99a8d7310bba5ee6dcfc74cd90217a3e8
HYE0139 <h.ye011096@gamil.com>
2022-06-16 13:07:21 +0900

    soket 통신

--- ch13.socket/tcp_server.php
+++ ch13.socket/tcp_server.php
@@ -2,17 +2,17 @@
     $addr = gethostbyname("127.0.0.1");
     $port = 5092;
     print "addr : {$addr}<br>";
-    set_time_limit(100);//인터넷프로토콜,소켓의종류,
-    if(($sock = socket_create(AF_INET, SOCK_STREAM, 0)) < 0) {
+    set_time_limit(100);//인터넷프로토콜,전송타입,
+    if(($sock = socket_create(AF_INET, SOCK_STREAM, 0)) < 0) { //통신 프로토콜 종류 설정
         echo "socket_create() failed: reason : " . socket_strerror($sock) . "<br>";
     } 
-    if(($ret = socket_bind($sock, $addr, $port) < 0)) {
+    if(($ret = socket_bind($sock, $addr, $port) < 0)) { // 주소 및 포트 할당
         echo "socket_bind() failed: reason : " . socket_strerror($ret) . "<br>";
     }
-    if(($ret = socket_listen($sock, 0)) == false) {
+    if(($ret = socket_listen($sock, 0)) == false) { // 연결요청대기
         echo "socket_listen() failed: reason : " . socket_strerror($ret) . "<br>";
     }
-    $msgsock = socket_accept($sock) or die ("accept failed");
+    $msgsock = socket_accept($sock) or die ("accept failed");//연결수락
     echo "Server is ready <br>";
      
 
