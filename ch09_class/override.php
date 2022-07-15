9f23cd872dc41d9ff7b7522cf47f47ee014e595f
HYE0139 <h.ye011096@gmail.com>
2022-07-15 11:06:33 +0900

    오버라이딩

---
+++ ch09_class/override.php
@@ -0,0 +1,47 @@
+<?php
+    class People {
+        protected $name;
+        protected $age;
+
+        function printPeople() {
+            print "Name : ".$this->name."<br>";
+            print "Age : ".$this->age."<br>";
+        }
+    }
+
+    class Student extends People {
+        private $studentId;
+
+        function __construct($name, $age, $id){
+            $this->name = $name;
+            $this->age = $age;
+            $this->studentId = $id;
+        }
+        function printPeople(){
+            print "--Student--<br>";
+            parent::printPeople();
+            print "Id : ".$this->studentId."<br>";
+        }
+    }
+
+    class Professor extends People {
+        private $office_No;
+
+        function __constuct($name, $age, $no){
+            $this->name = $name;
+            $this->age = $age;
+            $this->office_no = $no;
+        }
+        function printPeople() {
+            print "--Professor--<br>";
+            parent::printPeople();
+            print "Office_No : ".$this->office_No."<br>";
+        }
+    }
+
+    $object1 = new Student("Lee", "21", "20170123");
+    $object2 = new Professor("Kim", "42", "107");
+
+    $object1->printPeople();
+    print "<br>";
+    $object2->printPeople();
