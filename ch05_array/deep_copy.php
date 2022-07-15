d7843d5a9b930647999f9dd186dc685ab24861fb
HYE0139 <h.ye011096@gmail.com>
2022-07-15 10:57:53 +0900

    깊은 복사 이해

---
+++ ch05_array/deep_copy.php
@@ -0,0 +1,16 @@
+<?php
+  $score = array(1, 2, 3, 4);
+  $score2 = $score;
+
+  print_r($score);
+  echo "<br>";
+  print_r($score2);
+  echo "<br>";
+
+  $socre2[0] = 100;
+
+  print_r($socre);
+  echo "<br>";
+  print_r($score2);
+  echo "<br>";
+?>
