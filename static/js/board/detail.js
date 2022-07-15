f0d03519916a1094f0c1006451487ef5bc39cdc2
HYE0139 <h.ye011096@gmail.com>
2022-07-15 11:09:23 +0900

    0715

--- static/js/board/detail.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    const btnDel = document.getElementById('btnDel');
-    btnDel.addEventListener('click', ()=>{
-        if(confirm("삭제하시겠습니까?")){
-            location.href = `del?i_board=${btnDel.dataset.iboard}`
-        }
-    });
-})();
