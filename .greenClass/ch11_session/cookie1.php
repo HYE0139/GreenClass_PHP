a889779465c58d07f6dbe6f193b76e71df0f18c7
HYE0139 <h.ye011096@gmail.com>
2022-07-15 10:43:56 +0900

    사칙연산

--- .greenClass/ch11_session/cookie1.php
+++
@@ -1,4 +0,0 @@
-<?php
-  setcookie("country", "Korea");
-?>
-<a href="cookie2.php">Next Page</a>
--- .greenClass/ch11_session/cookie2.php
+++
@@ -1,9 +0,0 @@
-<?php
-  echo "country : ", $_COOKIE['country'], "<br>";
-
-  $_COOKIE['country'] = "UK";
-  echo "country : ", $_COOKIE['country'], "<br>";
-
-  unset($_COOKIE['country']);
-  echo "country : ", $_COOKIE['country'], "<br>";
-?>
